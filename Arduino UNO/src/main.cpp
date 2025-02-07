1e7b35dd78157cd8c0fa2b338f1187dcac79a527
emilyhix <hix.emily.m@gmail.com>
2025-02-06 18:28:09 -0800

    Change pin configuration for ultrasonic sensor

--- Arduino UNO/src/main.cpp
+++ Arduino UNO/src/main.cpp
@@ -1,5 +1,6 @@
 #include "timerISR.h"
 #include "helper.h"
+#include "serialATmega.h"
 
 #define NUM_TASKS 3
 
@@ -10,14 +11,20 @@ typedef struct _task{
 	int (*TickFct)(int); 		//Task tick function
 } task;
 
-//const unsigned long periodTask = number;
+const unsigned long SonarPeriod = 1;
+const unsigned long TakePicPeriod = 500;
+const unsigned long SendPicPeriod = 500;
 const unsigned long GCD_PERIOD = 1;
 
 task tasks[NUM_TASKS];
 
 enum Sonar_States {SONAR_INIT};
+enum TakePic_States {TAKE_PICTURE};
+enum SendPic_States {SEND_PICTURE};
 
 int TickFct_Sonar(int state);
+int TickFct_TakePic(int state);
+int TickFct_SendPic(int state);
 
 void TimerISR() {
     
@@ -32,24 +39,35 @@ void TimerISR() {
 
 int main(void) {
     
-    //Initialize PORTD
+    //Initialize PORTD as output
     DDRD = 0xFF;
     PORTD = 0x00;
-    //Initialize PORTC
-    DDRC = 0b11111100;
-    PORTC = 0b00000011;
-    //Initialize PORTB
+    //Initialize PORTC as output (PORTC2 - TRIG)
+    DDRD = 0xFF;
+    PORTD = 0x00;
+    //Initialize PORTB (PORTB0 - ECHO)
     DDRB = 0b11111110;
     PORTB = 0b00000001;
 
-    sonar_init(); // initializes sonar
+    serial_init(9600);
+    sonar_init();
 
-    // unsigned char i = 0;
-    // tasks[i].state = SONAR_INIT;
-    // tasks[i].period = periodSonar;
-    // tasks[i].elapsedTime = tasks[i].period;
-    // tasks[i].TickFct = &TickFct_Sonar;
-    // ++i;
+    unsigned char i = 0;
+    tasks[i].state = SONAR_INIT;
+    tasks[i].period = SonarPeriod;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].TickFct = &TickFct_Sonar;
+    ++i;
+    tasks[i].state = TAKE_PICTURE;
+    tasks[i].period = TakePicPeriod;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].TickFct = &TickFct_TakePic;
+    ++i;
+    tasks[i].state = SEND_PICTURE;
+    tasks[i].period = SendPicPeriod;
+    tasks[i].elapsedTime = tasks[i].period;
+    tasks[i].TickFct = &TickFct_SendPic;
+    ++i;
 
     TimerSet(GCD_PERIOD);
     TimerOn();
@@ -60,6 +78,38 @@ int main(void) {
 }
 
 int TickFct_Sonar(int state) {
+    switch (state) {
+        // STATE TRANSITIONS
+        case SONAR_INIT:
+            serial_println(sonar_read());
+            break;
+
+        default:
+            state = SONAR_INIT;
+            break;
+    }
+    switch (state) {
+        // STATE ACTIONS
+        case SONAR_INIT:
+            break;
+
+        default:
+            break;
+    }
+    return state;
+}
+
+int TickFct_TakePic(int state) {
+    switch (state) {
+        // STATE TRANSITIONS
+    }
+    switch (state) {
+        // STATE ACTIONS
+    }
+    return state;
+}
+
+int TickFct_SendPic(int state) {
     switch (state) {
         // STATE TRANSITIONS
     }
