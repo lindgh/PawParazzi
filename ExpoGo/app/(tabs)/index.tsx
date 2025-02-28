3a2fe45eff4aebec28480c3a2456e7bca349335a
Linda <lindaghun108@gmail.com>
2025-02-28 15:52:07 -0800

    added code to download image url from firebase and display

--- ExpoGo/app/(tabs)/index.tsx
+++ ExpoGo/app/(tabs)/index.tsx
@@ -1,13 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 import Slider from "@/components/Slider"
+import App from './display'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+// import storage from '@react-native-firebase/storage';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage'
+import { app } from '../../FirebaseConfig'
+import React, { useState, useEffect } from 'react'
 
 const Page = () => {
+    const [url, setUrl] = useState<string | undefined>("");
+
+    useEffect(() => {
+        const func = async () => {
+            const storage = getStorage();
+            const reference = ref(storage, 'images/photo.jpg');
+            await getDownloadURL(reference).then((x) => { setUrl(x) })
+        }
+
+        func();
+    }, []);
+
     return (
-        <View style = {styles.container}>
-            <Slider/>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Image
+                style={{ width: '70%', height: '70%' }}
+                source={{ uri: url }}
+            />
         </View>
-    )   
+    )
+    // return (
+    //     <View style = {styles.container}>
+    //         <Slider/>
+    //     </View>
+    // )   
 }
 
 export default Page
